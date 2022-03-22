4de6eedb259bb379add1de088b39f0c18cfd13b5
akhilaanand26 <98249341+akhilaanand26@users.noreply.github.com>
2022-03-22 13:03:20 +0530

    Add files via upload

---
+++ Calculate.java
@@ -0,0 +1,12 @@
+public class Calculate
+{
+public static void main(String args[])
+{
+int  a=10;
+int b=5;
+System.out.println("sum="+(a+b));
+System.out.println("sub="+(a-b));
+System.out.println("Div="+(a/b));
+System.out.println("mult="+(a*b));
+}
+}
---
+++ File.java
@@ -0,0 +1,8 @@
+public class File 
+{
+	public static void main(String args[])
+	{
+		String value="Akhila";
+		System.out.println("My name is" +value);
+	}
+}
